73ec3244309da1c45090f0439ea4410a3026cab4
newton.mallick <newton.mallick@celebaltech.com>
2024-12-27 16:07:57 +0530

    Added record `Common` related to Prompt Setting

--- ChatModel/Model/ChatModel.cs
+++ ChatModel/Model/ChatModel.cs
@@ -19,31 +19,46 @@
         public const string Ollama = "Ollama";
     }
 
-    public record AzureOpenAIOptions
+    public record AzureOpenAIOptions:Common
     {
         public string APIKey { get; set; }
         public string EndPoint { get; set; }
         public string Deployment { get; set; }
-        public int MaxRetries { get; set; }
 
     }
 
-    public record OpenAIOptions
+    public record OpenAIOptions:Common
     {
         public string APIKey { get; set; } = string.Empty;
         public string Deployment { get; set; }
     }
 
-    public record HFOption
+    public record HFOption:Common
     {
         public string ModelID { get; set; }
         public string URL { get; set; } = "https://api-inference.huggingface.co";
         public string ApiKey { get; set; }
+        public bool UseCache { get; set; }
+        public bool Wait { get; set; }
     }
-    public record OllamaOption
+    public record OllamaOption:Common
     {
         public string Model { get; set; } = "phi3";
         public string URL { get; set; } = "http://localhost::3000";
+    }
+
+    /// <summary>
+    /// Common Setting 
+    /// </summary>
+    public record Common
+    {
+        public double? Temperature { get; set; }
+        public int? MaxTokens { get; set; }
+        public int? MaxRetries { get; set; }
+        public double? TopP { get; set; }
+        public double? PresencePenality { get; set; }
+        public double? FrequencyPenalty { get; set; }
+        public IList<string>? Stop { get; set; }
     }
 
 
